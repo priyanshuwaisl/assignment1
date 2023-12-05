c27018dd351d547b6b1ed9bd3b91cbed6b4285af
priyanshuwaisl <152385187+priyanshuwaisl@users.noreply.github.com>
2023-12-05 10:34:40 +0530

    Array_15.java

--- src/in/waisl/assignent/Array_15.java
+++ src/in/waisl/assignent/Array_15.java
@@ -4,27 +4,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class Array_1way5 {
+public class Array_15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-//		ArrayList<Integer>list = new ArrayList<Integer>();
-		  ArrayList<Integer> list = new ArrayList<Integer>();
-		  
-		  //First 
-		  list.add(null);
-		  list.add(null);
-		  list.add(null);
-		  
-		  //Second
-		  ArrayList<Integer> list1 =(ArrayList<Integer>) Arrays.asList(3,75,6576,78756,768576,456);
-		  System.out.println(list1.toString());
-		  System.out.println(list1.toString());
-		
-		System.out.println("");
-		
+		 
+        ArrayList<String> ArrList = new ArrayList<String>();
+ 
+        
+        ArrList.add("Item 1");
+        ArrList.add("Item 2");
+        ArrList.add("Item 3");
+        ArrList.add("Item 4");
+        ArrList.add("Item 5");
+ 
+        // display Array List before swap
+        System.out.println("Before Swap the ArrayList ");
+        System.out.println(ArrList);
+ 
+        // Swapping the elements at 1 and 2 indices
+        Collections.swap(ArrList, 1, 2);
+ 
+        // display Array List after swap
+        System.out.println("After Swap the ArrayList");
+        System.out.println(ArrList);
 
 	}
 
 }
+
